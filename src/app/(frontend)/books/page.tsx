21cfe8e4c4825e9224003e7829f42014d01dd2db
vaagens <stevanvaagen@gmail.com>
2025-12-09 23:06:43 +0100

    laget basic book-side

--- src/app/(frontend)/books/page.tsx
+++ src/app/(frontend)/books/page.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import '../globals.css'
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import BookCard from '@/components/BookCard'
 
 export default async function HomePage() {
-  return (
-    <section>
+  try {
+    const payload = await getPayload({ config })
 
-    </section>
-  )
+    const { docs: books} = await payload.find({
+      collection: 'books',
+    })
+    return (
+      <div>
+        <section>Filtrering TODO</section>
+
+        <section>
+          {books.map(book => (
+            <BookCard key={book.id} book={book} />
+          ))}
+        </section>
+      </div>
+    )
+  } catch (error) {
+    console.error('Feeil ved henting av bøker', error)
+    return (
+      <div>
+        <p className="text-red-600">Kunne ikke laste bøker. Prøv igjen senere</p>
+      </div>
+    )
+  }
 }
