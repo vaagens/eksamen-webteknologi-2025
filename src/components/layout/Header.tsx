f290b4aa4089ea85fc341d7542bc0f2cd5722a79
vaagens <stevanvaagen@gmail.com>
2025-12-09 14:26:01 +0100

    Logo navn styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,9 +5,11 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <Link href="/">
+        <Link href="/" className="flex items-center gap-2">
           <Image src="/images/logo.svg" alt="Bookdragons Logo" width={40} height={40} />
+          <span className="text-xl font-bold">Bookdragons</span>
         </Link>
+
       </nav>
     </header>
   )
