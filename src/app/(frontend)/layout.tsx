e1472c78e945155c87f985c3d64f755a59e5918f
vaagens <stevanvaagen@gmail.com>
2025-12-09 14:20:00 +0100

    Lagt på bakgrunnsfarge på body

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Metadata } from 'next'
-import './global.css'
+import './globals.css'
+import Header from '@/components/layout/Header'
 
 export const metadata: Metadata = {
   description: 'BookDragoons',
@@ -12,7 +13,8 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="nb-NO">
-      <body>
+    <body className="min-h-screen bg-gradient-to-br from-yellow-50 via-orange-200 to-red-300 text-gray-900">
+        <Header/>
         <main>{children}</main>
       </body>
     </html>
