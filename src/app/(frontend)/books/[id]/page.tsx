1590bd182fd73d09d0a67d1a7f14540386714139
Stevan M. Vågen <59422215+vaagens@users.noreply.github.com>
2025-12-10 14:34:48 +0100

    Merge pull request #77
    * Lag app/books/[id]/page.tsx

---
+++ src/app/(frontend)/books/[id]/page.tsx
@@ -0,0 +1,90 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import { AgeGroup, Author, Genre, Media } from '@/payload-types'
+import Image from 'next/image'
+
+export default async function BookPage({ params }: { params: { id: string } }) {
+  const payload = await getPayload({ config })
+
+  try {
+    const book = await payload.findByID({
+      collection: 'books',
+      id: params.id,
+      depth: 2,
+    })
+
+    const authors = book.author as Author[]
+    const cover = book.coverImage as Media
+    const genres = book.genres as Genre[]
+    const age = book.ageGroup as AgeGroup | null
+
+    return (
+      <article className="flex flex-col items-center gap-6 m-2">
+
+        <header>
+          <h1 className="text-3xl font-bold">{book.title}</h1>
+        </header>
+
+        <section className="flex items-baseline gap-3">
+          <h2 className="text-xl font-bold">Forfatter:</h2>
+          <p>{authors.map((a) => a.name).join(', ')}</p>
+        </section>
+
+        <section className="relative w-64 h-96">
+          <Image
+            src={cover?.sizes?.card?.url || ''}
+            alt={book.title}
+            fill
+            className="object-cover rounded shadow-lg"
+          />
+        </section>
+
+        <section className="text-center max-w-prose">
+          <h2 className="text-xl font-bold">Beskrivelse:</h2>
+          <p>{book.description}</p>
+        </section>
+
+        <section className="max-w-prose text-center">
+          <h2 className="text-xl font-bold">Detaljer:</h2>
+          <div className="flex items-baseline gap-3">
+            <dt className="font-semibold">Sjanger:</dt>
+            <dd>{genres.map((g) => g.name).join(', ')}</dd>
+          </div>
+          <div className="flex items-baseline gap-3">
+            <dt className="font-semibold">Aldersgruppe:</dt>
+            <dd>{age?.ageGroup || 'Ukjent aldersgruppe'}</dd>
+          </div>
+          <div className="flex items-baseline gap-3">
+            <dt className="font-semibold">ISBN:</dt>
+            <dd>{book.isbn}</dd>
+          </div>
+        </section>
+
+        <section className="flex items-baseline gap-6">
+          <div className="flex items-baseline gap-1">
+            <dt className="font-semibold">Pris:</dt>
+            <dd>{book.price} kr</dd>
+          </div>
+          <div className="flex items-baseline gap-1">
+            <dt className="font-semibold">Lagerstatus:</dt>
+            <dd>{book.stock}</dd>
+          </div>
+        </section>
+
+        <section className="flex items-baseline gap-6">
+          kanpper her
+        </section>
+
+      </article>
+    )
+  } catch (error) {
+    return (
+      <div className="p-10 text-center">
+        <h1 className="text-2xl font-semibold">Fant ikke boken</h1>
+        <p className="text-gray-500 mt-2">Det finnes ingen bok med denne adressen.</p>
+        <p>TILBAKE KNAPP HER</p>
+      </div>
+
+    )
+  }
+}
